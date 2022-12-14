b1889cbe025d85bc7c4d00210c44e7f5b9303bd1
Zain_Ali_Sidd <114559080+Zain-Ali-Siddiqui@users.noreply.github.com>
2022-12-14 13:06:32 +0500

    update

--- src/Screen/Login.jsx
+++ src/Screen/Login.jsx
@@ -95,7 +95,7 @@ function Login({value}) {
                             <button className="button" onClick={LoginForm}> Register </button>
                         </Link>
                 }
-                <p className='para'>Don't Have An Account ? <Link to="/Sidebar">Sign Up</Link> </p>
+                <p className='para'>Don't Have An Account ? <Link to="/">Sign Up</Link> </p>
                 {/* <div>
                     <GoogleLogin />
                 </div> */}
